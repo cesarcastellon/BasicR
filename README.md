e8fbb07819a0828c68f4da55d4c9773f64495ea9
Cesar Castellon <ceccastel@gmail.com>
2020-04-28 18:40:46 -0500

    second commit from Rstudio

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## This line was written in Github
 
 ### this line too
+
+
+# Goal: reproduce analysis of municipal elections
--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -3,6 +3,14 @@
 install.packages("readstata13")
 
 library(readstata13)
-dat <- read.dta13("electoral_data_colombia2.dta")
+
+dat <- read.dta13("data/electoral_data_colombia2.dta")
+muni <- read.dta13("data/colombia_municipal_panel_data_CEDE_multiple.dta")
 
 summary(dat)
+
+
+
+
+
+
